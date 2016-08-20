14ddd560d9bf3ece59c1c9916640d60da1c09811
Gildarytzs <memepasenreve@hotmail.fr>
2016-08-20 19:12:28 +0200

    Update coodmin.sql

--- coodmin.sql
+++ coodmin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 04 Août 2016 à 21:00
+-- Généré le :  Sam 20 Août 2016 à 19:11
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `bids` (
   `verified` tinyint(1) DEFAULT '0',
   `sold` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Contenu de la table `bids`
@@ -54,7 +54,11 @@ INSERT INTO `bids` (`id`, `seller`, `title`, `descriptive`, `category`, `estimat
 (8, 10, 'Parvati', 'Msi neuf importante', 3, 2147483647, 2147483647, 3600, 1469193004, '../img/bids/0.jpg', 1, 0),
 (9, 10, 'Bjkjvnlkejlbovki', 'Vjknewbvnkewjnvkwjentv', 3, 57, 57, 3600, 1469194465, '../img/bids/0.jpg', 1, 0),
 (10, 10, 'Test 1', 'Test 1', 1, 1, 1, 86400, 1469297717, '../img/bids/0.jpg', 1, 0),
-(11, 10, 'Test 2', 'Test 2', 1, 2, 2, 172800, 1469384118, '../img/bids/0.jpg', 1, 0);
+(11, 10, 'Test 2', 'Test 2', 1, 2, 2, 172800, 1469384118, '../img/bids/0.jpg', 1, 0),
+(12, 0, 'Voiture a vendre', 'Neuve pas cher', 1, 500, 450, 1382400, 1473082564, '../img/bids/0.jpg', 1, 0),
+(13, 0, 'Gjghf', 'Dfhdfghd', 5, 1, 1, 60, 1471700304, '../img/bids/0.jpg', 1, 0),
+(14, 0, 'Rfvrevregv', 'Zfzrgvzervb', 5, 12, 14, 60, 1471712363, '../img/bids/0.jpg', 1, 0),
+(15, 11, 'Zefdzef', 'Zefvzfzv', 1, 23, 52, 120, 1471713051, '../img/bids/0.jpg', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -111,23 +115,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `access_token` char(32) NOT NULL,
   `admin` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Contenu de la table `users`
 --
 
 INSERT INTO `users` (`id`, `type`, `surname`, `name`, `pseudo`, `email`, `tel`, `password`, `access_token`, `admin`) VALUES
-(1, 'Mme', 'Pute', 'Asas', 'salutsalut', 'salut@salut.fr', '', '$2y$10$m671YPW9P.2AQLbnNfeZtOLFirQCiiQi6Kxqf/05.JPsZBtHO4uwS', '8a89029280f8fcb0c77ad7358f0cd834', 0),
-(2, 'Mr', 'Monsieur', 'Monsiedu', 'Monon', 'monmon@htoa.fr', '0695688618', '$2y$10$ZFjwQKIa.LBSBfebde8dcOV93mjkfd41bCbENzbW3bfZLYZeFI5jK', '4bd5b938bb6a821ac9a88c97fff9c2ba', 0),
-(3, 'Mr', 'Monsieur', 'Monsiedu', 'Monon', 'monmon@htoa.fr', '0695688618', '$2y$10$HLydjZhlxjT3goZPd69oR.zS4x2Zxu1nSh1Kb8gIv.Ghl9.UMrDTi', '3d5344e70fb1b68726b8c4b4b62cafaf', 0),
-(4, 'Mr', 'Monsieur', 'Monsiedu', 'Monon', 'monmon@htoa.fr', '0695688618', '$2y$10$E8kUoSe4JoRI8tBoOAPUieBgyVrZEanhxRecnhccyMF2AAJp5gH4e', '497062f43996bb69f80bf2a435d1d6c4', 0),
-(5, 'Mme', 'Qeffe', 'Qfdf', 'Qfdqfqf', 'monmon@htoa.fr', '0695688618', '$2y$10$rgMfDmJ6XZBmTqCVH1izs.h4hAa1PRu7DcNJYzP7fTH8Ru4vNJZHC', '75df34f1feaa23bebb4076a8fd1213e8', 0),
-(6, 'Mr', 'Wew', 'Wef', 'Wfwe', 'salut@salut.fr', '0695688618', '$2y$10$ADKEdN6oKbX/pVii3RZKw.iZF4.8q6WyLwNarWtsrkEefxrxvhrZe', '8a89029280f8fcb0c77ad7358f0cd834', 0),
-(7, 'Mme', 'Wew', 'Wef', 'Wfwe', 'salut@salut.fr', '0695688618', '$2y$10$mNKzZdYGi60yW2kyzIpKyuZRc3LCxVNlRM.xGkDBQOswAFnSP.e1m', '8a89029280f8fcb0c77ad7358f0cd834', 0),
-(8, 'Mr', 'Salut', 'Salut', 'Salut', 'salut@salut.fr', '0695688618', '$2y$10$As0IGqpg2/190i.PogqIfOuvZwxgJofnU2556kPwsnLey6n1m/mEe', '8a89029280f8fcb0c77ad7358f0cd834', 0),
-(9, 'Mr', 'Salut', 'Salut', 'Salut', 'salut@salut.fr', '0695688618', '$2y$10$jCktlx7rJgWwti.BXgNSCeec5TEHuaYYeI/WhXLF4JVKHEBsn1jx.', '8a89029280f8fcb0c77ad7358f0cd834', 0),
-(10, 'Mr.', 'Swann', 'Kaulanjan', 'Gildarytzs', 'swann.kaulanjan@laposte.net', '0695688618', '$2y$10$mQRJEeqnGfTimoTPzg57Xe/zl8UXAS.9UwoTncHVuULLwY0J5HwPK', '55cb125e48065bc82b3588b66f36aff7', 1);
+(0, 'Mr.', 'Swann', 'Kaulanjan', 'Gildarytzs', 'swann.kaulanjan@laposte.net', '0695688618', '$2y$10$mQRJEeqnGfTimoTPzg57Xe/zl8UXAS.9UwoTncHVuULLwY0J5HwPK', '1ad802f0cd71fa9f6b7ea283b1bf2e3f', 1),
+(11, 'Mr.', 'Jean', 'Piere', 'Piere', 'pierre@hot.com', '0149501850', '$2y$10$hUjhmEvrKlqnDM1HXT/Ob.8oge5mZkrfV668tvpEsxyOIKABfhWf2', '68e6a4e8f7a76ec4d41fbb8f4b1f6e6e', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
